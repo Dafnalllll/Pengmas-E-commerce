dc3292f64c7e346a37f995d2ad1652434835345f
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2025-08-27 09:53:03 +0700

    pengmasupdet

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<!-- filepath: d:\Dafa Code\pengmase-ecommerce\resources\views\components\navbar.blade.php -->
+<nav class="fixed top-0 left-0 w-full z-50 bg-[#A52A2A] bg-opacity-90 flex items-center justify-between px-20 py-4 shadow-md">
+    <!-- Logo -->
+    <div class="flex items-center">
+        <img src="/images/niranta-logo.png" alt="Niranta Logo" class="h-12 mr-4">
+        <span class="text-xs text-[#1A120B] font-medium">Gula Aren Asli</span>
+    </div>
+    <!-- Menu -->
+    <ul class="flex space-x-8">
+        <li>
+            <a href="/" class="font-bold text-[#1A120B] hover:text-black">HOME</a>
+        </li>
+        <li>
+            <a href="/product" class="font-semibold text-[#5C4B36] hover:text-black">PRODUCT</a>
+        </li>
+        <li>
+            <a href="/recipe" class="font-semibold text-[#5C4B36] hover:text-black">RECIPE</a>
+        </li>
+        <li>
+            <a href="/blogs" class="font-semibold text-[#5C4B36] hover:text-black">BLOGS</a>
+        </li>
+        <li>
+            <a href="/contact" class="font-semibold text-[#5C4B36] hover:text-black">CONTACT US</a>
+        </li>
+    </ul>
+</nav>
