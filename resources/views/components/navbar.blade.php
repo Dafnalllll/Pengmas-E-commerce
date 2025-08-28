4ba2842285b7a6be0210f1e30fc3e87020bfad7e
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2025-08-28 08:23:19 +0700

    pengupt

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,32 +1,42 @@
 <!-- filepath: d:\Dafa Code\pengmase-ecommerce\resources\views\components\navbar.blade.php -->
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Cormorant+Garamond:wght@400;700&display=swap" rel="stylesheet">
+</head>
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.4/dist/aos.css" />
 <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
 <nav 
-  class="fixed top-0 left-0 w-full z-50 bg-[#A52A2A] bg-opacity-90 flex items-center justify-between px-20 py-4 shadow-md"
-  data-aos="fade-down"
-  data-aos-duration="800"
+    class="fixed top-0 left-0 w-full z-50 bg-[#A52A2A] bg-opacity-90 flex items-center justify-between px-20 py-4 shadow-md"
+    data-aos="fade-down"
+    data-aos-duration="800"
 >
-    <!-- Logo -->
-    <div class="flex items-center">
-        <img src="/img/ikonrasaumbi.png" alt="Niranta Logo" class="h-12 mr-4">
-        <span class="text-xs text-[#E0E0E0] font-[Playfair Display] font-medium">Rasa Umbi</span>
+    <!-- Logo, Judul, Tagline -->
+    <div class="flex items-center space-x-4">
+        <img src="/img/ikonrasaumbi.png" alt="Rasa Umbi Logo" class="h-12 w-auto drop-shadow-lg">
+        <div class="flex flex-col">
+            <span class="text-xl font-semibold text-[#E0E0E0]" style="font-family: 'Cormorant Garamond', serif;">
+                Rasa Umbi
+            </span>
+            <span class="text-lg text-white font-light tracking-wide mt-1" style="font-family: 'Great Vibes', cursive;">
+                Rasa yang tumbuh dari bumi nusantara
+            </span>
+        </div>
     </div>
     <!-- Menu -->
     <ul class="flex space-x-8">
         <li>
-            <a href="/" class="font-bold text-[#1A120B] hover:text-black">HOME</a>
+            <a href="/" class="font-bold text-white hover:text-[#FFD6C0] transition">HOME</a>
         </li>
         <li>
-            <a href="/product" class="font-semibold text-[#5C4B36] hover:text-black">PRODUCT</a>
+            <a href="/product" class="font-semibold text-white/80 hover:text-[#FFD6C0] transition">PRODUCT</a>
         </li>
         <li>
-            <a href="/recipe" class="font-semibold text-[#5C4B36] hover:text-black">RECIPE</a>
+            <a href="/recipe" class="font-semibold text-white/80 hover:text-[#FFD6C0] transition">RECIPE</a>
         </li>
         <li>
-            <a href="/blogs" class="font-semibold text-[#5C4B36] hover:text-black">BLOGS</a>
+            <a href="/blogs" class="font-semibold text-white/80 hover:text-[#FFD6C0] transition">BLOGS</a>
         </li>
         <li>
-            <a href="/contact" class="font-semibold text-[#5C4B36] hover:text-black">CONTACT US</a>
+            <a href="/contact" class="font-semibold text-white/80 hover:text-[#FFD6C0] transition">CONTACT US</a>
         </li>
     </ul>
 </nav>
